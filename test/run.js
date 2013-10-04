7349ab36371fd7c7d46d3fc672d25845555f275d
Ben Newman <bn@cs.stanford.edu>
2013-10-04 11:06:23 -0400

    Add some tests of the NodePath abstraction.

--- test/run.js
+++ test/run.js
@@ -6,6 +6,8 @@ var fs = require("fs");
 var esprima = require("esprima");
 var esprimaSyntax = esprima.Syntax;
 var parse = esprima.parse;
+var Path = require("ast-path").Path;
+var NodePath = require("../lib/node-path");
 
 exports.testBasic = function(t, assert) {
     var fooId = b.identifier("foo");
@@ -318,6 +320,40 @@ exports.testTraverse = function(t, assert) {
         foo: true,
         bar: true
     });
+
+    t.finish();
+};
+
+exports.testNodePath = function(t, assert) {
+    assert.strictEqual(new Path({}).constructor, Path);
+
+    var np = new NodePath(b.identifier("foo"));
+    assert.strictEqual(np.constructor, NodePath);
+    assert.ok(np.get("name") instanceof NodePath);
+
+    var ast = b.expressionStatement(
+        b.unaryExpression("!", b.sequenceExpression([
+            b.identifier("a"),
+            b.identifier("b"),
+            b.identifier("c")
+        ]))
+    );
+
+    var path = new NodePath(ast);
+
+    var opPath = path.get("expression", "operator");
+    assert.strictEqual(opPath.value, "!");
+    assert.strictEqual(opPath.node, ast.expression);
+    assert.strictEqual(opPath.parent, path);
+    assert.strictEqual(opPath.parent.node, ast);
+
+    var argPath = path.get("expression", "argument");
+    assert.ok(argPath.needsParens());
+
+    var exprsPath = argPath.get("expressions");
+    assert.ok(!exprsPath.needsParens());
+    assert.strictEqual(exprsPath.get("length").value, 3);
+    assert.ok(!exprsPath.get(1).needsParens());
 
     t.finish();
 };
