b3fb8bab44132c5384e8b571686a7fb0ee0e7442
Ben Newman <bn@cs.stanford.edu>
2013-11-06 12:28:40 -0500

    Handle CatchClause scope subtleties correctly, with tests.

--- test/run.js
+++ test/run.js
@@ -455,3 +455,48 @@ exports.testGlobalScope = function(t, assert) {
 
     t.finish();
 };
+
+var catchWithVarDecl = [
+    "function foo(e) {",
+    "  try {",
+    "    bar();",
+    "  } catch (e) {",
+    "    var f = e + 1;",
+    "    return function(g) {",
+    "      return e + g;",
+    "    };",
+    "  }",
+    "  return f;",
+    "}"
+];
+
+exports.testCatchScope = function(t, assert) {
+    var path = new NodePath(parse(catchWithVarDecl.join("\n")));
+
+    var fooPath = path.get("body", 0);
+    var fooScope = fooPath.scope;
+    n.FunctionDeclaration.assert(fooScope.node);
+    assert.strictEqual(fooScope.declares("e"), true);
+    assert.strictEqual(fooScope.declares("f"), true);
+    assert.strictEqual(fooScope.lookup("e"), fooScope);
+
+    var catchPath = fooPath.get("body", "body", 0, "handler");
+    var catchScope = catchPath.scope;
+    n.CatchClause.assert(catchScope.node);
+    assert.strictEqual(catchScope.declares("e"), true);
+    assert.strictEqual(catchScope.declares("f"), false);
+    assert.strictEqual(catchScope.lookup("e"), catchScope);
+    assert.strictEqual(catchScope.lookup("f"), fooScope);
+
+    var closurePath = catchPath.get("body", "body", 1, "argument");
+    var closureScope = closurePath.scope;
+    n.FunctionExpression.assert(closureScope.node);
+    assert.strictEqual(closureScope.declares("e"), false);
+    assert.strictEqual(closureScope.declares("f"), false);
+    assert.strictEqual(closureScope.declares("g"), true);
+    assert.strictEqual(closureScope.lookup("g"), closureScope);
+    assert.strictEqual(closureScope.lookup("e"), catchScope);
+    assert.strictEqual(closureScope.lookup("f"), fooScope);
+
+    t.finish();
+};
