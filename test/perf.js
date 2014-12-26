6a09f7eb4e9f99b7a052b72afdaffae73a7e6eea
DrSlump <drslump@pollinimini.net>
2014-12-26 21:45:31 +0100

    iteration based visitor with meta-programming optimizations

--- test/perf.js
+++ test/perf.js
@@ -1,6 +1,6 @@
 var path = require('path');
 var fs = require('fs');
-var visit = require("ast-types").visit;
+var visit = require("../").visit;
 var parse = require("esprima").parse;
 
 var backbone = fs.readFileSync(
@@ -10,15 +10,47 @@ var backbone = fs.readFileSync(
 
 var ast = parse(backbone);
 
-var names = [];
-var start = +new Date;
-
-visit(ast, {
-  visitNode: function(path) {
-    names.push(path.name);
-    this.traverse(path);
+function timeit(title, cnt, fn) {
+  var times = [];
+  var names = [];
+  for (var i=0; i<cnt; i++) {
+    var start = Date.now();
+    fn(names);
+    times.push(Date.now() - start)
   }
+  var avg = times.reduce(function (a, b) { return a + b; }) / times.length;
+  console.log('%s: [%s] avg %d (%d)', title, times.join(', '), avg, names.length);
+}
+
+
+var CNT = 5;
+
+
+timeit('NodePath', CNT, function (names) {
+  visit(ast, {
+    visitNode: function(path) {
+      names.push(path.name);
+      this.traverse(path);
+    }
+  });
 });
 
-console.log(names.length);
-console.log(new Date - start, "ms");
+var MyVisitor = require('../').Visitor.create();
+timeit('Visitor', CNT, function (names) {
+  MyVisitor.visit(ast, {
+    visitNode: function(node) {
+      names.push(node.type);
+    }
+  });
+});
+
+var namesRef;
+var precompiled = MyVisitor.fromMethodsObject({
+  visitNode: function(node) {
+    namesRef.push(node.type);
+  }
+})
+timeit('Precompiled', CNT, function (names) {
+  namesRef = names;
+  precompiled.visit(ast);
+})
