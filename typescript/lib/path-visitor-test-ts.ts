1548adaae7d57b6bcd13a5dce0a35d14b92bcda9
James Talmage <james@talmage.io>
2015-04-17 01:13:35 -0400

    tests for path-visitor.d.ts

---
+++ typescript/lib/path-visitor-test-ts.ts
@@ -0,0 +1,52 @@
+/// <reference path="path-visitor.d.ts"/>
+
+// call this and cast to the desired type
+function magic(...params:any[]):any{}
+
+// validate member properties and function return types by assigning to these variables
+var boolean: boolean;
+var string: string;
+var number: number;
+var path: AstTypes.NodePathInstance;
+var ctx: AstTypes.PathVisitorContext;
+var methods: AstTypes.PathVisitorMethods;
+
+// test subjects
+var PathVisitor: AstTypes.PathVisitorStatic = magic();
+var pathVisitor: AstTypes.PathVisitorInstance = magic();
+
+// Static methods and constructor
+pathVisitor = new PathVisitor();
+pathVisitor = PathVisitor.fromMethodsObject({});
+pathVisitor = PathVisitor.fromMethodsObject(pathVisitor);
+
+path = PathVisitor.visit({}, {
+  visitReturnStatement: (path:AstTypes.NodePathInstance) => {
+    path = this.traverse(path, pathVisitor);
+  }
+});
+
+ctx.abort();
+path = ctx.visit(path);
+path = ctx.visit(path, {});
+path = ctx.traverse(path);
+path = ctx.traverse(path, {});
+path = ctx.traverse(path, pathVisitor);
+string = ctx.invokeVisitorMethod('hello');
+boolean = ctx.invokeVisitorMethod('hello');
+ctx = ctx.reset(path);
+
+pathVisitor.visit(path);
+pathVisitor.visit(path, 'arg1', 3);
+
+var abortRequest = new pathVisitor.AbortRequest();
+abortRequest.cancel();
+
+pathVisitor.abort();
+
+pathVisitor.reset(path, 'arg1', 2, 'arg3');
+pathVisitor.visitWithoutRest(path);
+ctx = pathVisitor.acquireContext(path);
+pathVisitor.releaseContext(ctx);
+pathVisitor.reportChanged();
+boolean = pathVisitor.wasChangeReported();
--- typescript/lib/path-visitor.d.ts
+++ typescript/lib/path-visitor.d.ts
@@ -1,20 +1,47 @@
 /// <reference path="node-path.d.ts"/>
 declare module AstTypes {
 
-  type PathVisitor_visit = (node: any, methods: PathVisitorInstance) => NodePathInstance
-
   export interface PathVisitorStatic {
     new (): PathVisitorInstance
-    fromMethodsObject(methods: PathVisitorInstance): PathVisitorInstance
-    visit: PathVisitor_visit
+    fromMethodsObject(methods: PathVisitorMethods|PathVisitorInstance): PathVisitorInstance
+    visit: (node: any, methods: PathVisitorMethods) => NodePathInstance
+  }
+
+  export interface PathVisitorMethods {
+    // you never actually provide these methods, they are here so `this.traverse()` (etc)
+    // do not cause compilation errors in your visitor methods
+    abort?()
+    visit?(path: NodePathInstance, newVisitor?: PathVisitorMethods): NodePathInstance
+    traverse?(path: NodePathInstance, newVisitor?: PathVisitorInstance|PathVisitorMethods)
+    invokeVisitorMethod?(methodName: string):any
+    reset?(path: NodePathInstance):PathVisitorContext
+  }
+
+  export interface PathVisitorContext {
+    abort():void
+    visit(path: NodePathInstance, newVisitor?: PathVisitorMethods): NodePathInstance
+    traverse(path: NodePathInstance, newVisitor?: PathVisitorInstance|PathVisitorMethods)
+    invokeVisitorMethod(methodName: string):any
+    reset(path: NodePathInstance):PathVisitorContext
   }
 
   export interface PathVisitorInstance {
-    // reset?(path: Path): Instance
-    // invokeVisitorMethod?(methodName: string): NodeType
-    traverse?(path: NodePathInstance): NodePathInstance
-    visit?(path: any, newVisitor: PathVisitorInstance): NodePathInstance
-    // reportChanged?()
-    abort?()
+    visit(path: any, ...additionalArgs:any[]): any
+    AbortRequest:AbortRequestStatic
+    abort()
+    reset(path: NodePathInstance, ...additionalArgs:any[]): any
+    visitWithoutRest(path: NodePathInstance): any
+    acquireContext(path: NodePathInstance): PathVisitorContext
+    releaseContext(context: PathVisitorContext)
+    reportChanged()
+    wasChangeReported():boolean
+  }
+
+  export interface AbortRequestStatic {
+    new (): AbortRequestInstance
+  }
+
+  export interface AbortRequestInstance {
+    cancel()
   }
 }
