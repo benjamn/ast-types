efcb1e1adc86aec2ebe548c438b33f3c68b71acb
Francois Ward <fward@hubspot.com>
2018-02-01 23:02:54 -0500

    Some progress on complex type declarations

--- def/typescript.js
+++ def/typescript.js
@@ -3,23 +3,46 @@ module.exports = function (fork) {
 
   var types = fork.use(require("../lib/types"));
   var def = types.Type.def;
+  var or = types.Type.or;
+
+  def("TSType")
+    .bases("Node");
 
   def("TSNumberKeyword")
-    .bases("Node")
+    .bases("TSType")
     .build();
 
-  def("TSTypeParameter")
+  def("TSTypeReference")
+    .bases("TSType")
+    .field("typeName", def("Identifier"));
+
+  def("TSTypeAnnotation")
     .bases("Node")
-    .build("name")
-    .field("name", def("Identifier"))
+    .field("typeAnnotation", def("TSType"))
+    .build();
+
+  def("TSPropertySignature")
+    .bases("Node")
+    .build("key")
+    .field("key", def("Identifier"))
+    .field("computed", or(Boolean), false)
+    .field("typeAnnotation", def("TSTypeAnnotation"))
+
+  def("TSTypeLiteral")
+    .bases('TSTypeAnnotation')
+    .field('members', [def("TSPropertySignature")]);
+
+  def("TSTypeParameter")
+    .bases("Identifier");
+
+  def("TSTypeParameterDeclaration")
+    .bases("Declaration")
+    .build("params")
+    .field("params", [def("TSTypeParameter")]);
 
   def("TSTypeAliasDeclaration")
     .bases("Declaration")
     .build("id")
     .field("id", def("Identifier"))
-    .field("typeAnnotation", def("Identifier"));
-
-  def("TSTypeParameterDeclaration")
-    .bases("Declaration")
-    .field("params", def("Identifier"));
+    .field("typeAnnotation", def("TSType"));
 };
