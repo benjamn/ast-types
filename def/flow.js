4418dfe279b480884beb1d83363d72837b47f2f9
Benjamin Woodruff <bgw@fb.com>
2018-02-16 14:16:30 -0800

    Add type parameter fields to classes (fixes #242)
    Adds typeParameter and superTypeParameters to ClassExpression and
    ClassDeclaration.

--- def/flow.js
+++ def/flow.js
@@ -242,6 +242,22 @@ module.exports = function (fork) {
     .field("static", Boolean, defaults["false"])
     .field("variance", LegacyVariance, defaults["null"]);
 
+  def("ClassDeclaration")
+    .field("typeParameter",
+           or(def("TypeParameterDeclaration"), null),
+           defaults["null"])
+    .field("superTypeParameters",
+           or([def("GenericTypeAnnotation")], null),
+           defaults["null"]);
+
+  def("ClassExpression")
+    .field("typeParameter",
+           or(def("TypeParameterDeclaration"), null),
+           defaults["null"])
+    .field("superTypeParameters",
+           or([def("GenericTypeAnnotation")], null),
+           defaults["null"]);
+
   def("ClassImplements")
     .bases("Node")
     .build("id")
