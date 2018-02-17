8ece8513a83e24c2517787b74448eeaccb92874e
Ben Newman <ben@benjamn.com>
2018-02-17 16:23:57 -0500

    Test expected parsing failures.

--- test/typescript.js
+++ test/typescript.js
@@ -1,3 +1,4 @@
+var assert = require("assert");
 var fs = require("fs");
 var path = require("path");
 var shared = require("./shared.js");
@@ -19,25 +20,72 @@ require("glob")("**/input.js", {
       var fullPath = path.join(tsRootDir, tsPath);
 
       it("should validate " + path.relative(pkgRootDir, fullPath), function (done) {
-        fs.readFile(fullPath, "utf8", function (err, code) {
-          if (err) {
-            throw err;
+        fs.readFile(fullPath, "utf8", function (error, code) {
+          if (error) {
+            throw error;
           }
-
-          var program = require("babylon").parse(code, {
-            sourceType: "module",
-            plugins: [
-              "typescript",
-              "classProperties",
-              "decorators",
-            ]
-          }).program;
-
-          tsTypes.namedTypes.Program.assert(program, true);
-
+          var program = tryParse(code, fullPath);
+          if (program !== null) {
+            tsTypes.namedTypes.Program.assert(program, true);
+          }
           done();
         });
       });
     });
   });
 });
+
+function tryParse(code, fullPath) {
+  var parseOptions = getOptions(fullPath);
+
+  try {
+    return require("babylon").parse(code, parseOptions).program;
+
+  } catch (error) {
+    // If parsing fails, check options.json to see if the failure was
+    // expected.
+    try {
+      var options = JSON.parse(fs.readFileSync(
+        path.join(path.dirname(fullPath), "options.json")));
+    } catch (optionsError) {
+      console.error(optionsError.message);
+    }
+
+    if (options &&
+        options.throws === error.message) {
+      return null;
+    }
+
+    throw error;
+  }
+}
+
+function getOptions(fullPath) {
+  var plugins = getPlugins(path.dirname(fullPath));
+  return {
+    sourceType: "module",
+    plugins,
+  };
+}
+
+function getPlugins(dir) {
+  try {
+    var options = JSON.parse(fs.readFileSync(
+      path.join(dir, "options.json")
+    ));
+  } catch (ignored) {
+    options = {};
+  }
+
+  if (options.plugins) {
+    return options.plugins;
+  }
+
+  if (dir !== tsRootDir) {
+    return getPlugins(path.dirname(dir));
+  }
+
+  return [
+    "typescript",
+  ];
+}
