08225344e2127c223ae64fedce65176c6c4ed00c
Ben Newman <ben@benjamn.com>
2018-03-09 10:25:11 -0500

    Check out v2.7.2 of TypeScript rather than latest master.
    This should help prevent test failures due to the latest published version
    of Babylon lagging behind the latest TypeScript development.

--- test/run.sh
+++ test/run.sh
@@ -6,18 +6,21 @@ cd $(dirname $0)/data
 
 BAB_TAG=v$(node -p 'require("babylon/package.json").version')
 
-if [ ! -d babylon-typescript-fixtures ]
+if [ ! -d babylon ]
 then
     git clone --branch "$BAB_TAG" --depth 1 \
         https://github.com/babel/babel.git
-    mv babel/packages/babylon/test/fixtures/typescript \
-       babylon-typescript-fixtures
+    mv babel/packages/babylon .
     rm -rf babel
 fi
 
+# Hard-code this for now.
+TS_TAG=v2.7.2
+
 if [ ! -d typescript-compiler ]
 then
-    git clone --depth 1 https://github.com/Microsoft/TypeScript.git
+    git clone --branch "$TS_TAG" --depth 1 \
+        https://github.com/Microsoft/TypeScript.git
     mv TypeScript/src/compiler typescript-compiler
     rm -rf TypeScript
 fi
