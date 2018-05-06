b0255f586acfe681a97b5227172d90a9283ec8bc
Ger Hobbelt <ger@hobbelt.com>
2018-05-06 18:54:31 +0200

    keeping our own leak-checking test parameters in the test script.

--- test/run.sh
+++ test/run.sh
@@ -6,6 +6,10 @@ cd $(dirname $0)/data
 
 if [ ! -d babylon-typescript-fixtures ]
 then
+	if [ -d babel ]
+	then
+    	rm -rf babel
+	fi
     git clone --depth 1 https://github.com/babel/babel.git
     mv babel/packages/babylon/test/fixtures/typescript \
        babylon-typescript-fixtures
@@ -14,6 +18,10 @@ fi
 
 if [ ! -d typescript-compiler ]
 then
+	if [ -d TypeScript ]
+	then
+    	rm -rf TypeScript
+    fi
     git clone --depth 1 https://github.com/Microsoft/TypeScript.git
     mv TypeScript/src/compiler typescript-compiler
     rm -rf TypeScript
