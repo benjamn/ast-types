f83281102fc9cb7e41f22932439d42b28a5141fb
Brie <723547+brieb@users.noreply.github.com>
2018-11-18 14:56:48 -0800

    Script for cleaning emitted files

---
+++ script/clean.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const glob = require("glob");
+
+const ext = "{js,js.map,d.ts,d.ts.map}";
+
+glob.sync("{def,lib}/**/*." + ext)
+  .concat(glob.sync("test{*,/!(data)/**}/*." + ext))
+  .concat(glob.sync("{fork,main}." + ext))
+  .forEach(filename => {
+    if (fs.existsSync(filename)) {
+      fs.unlinkSync(filename);
+    }
+  });
