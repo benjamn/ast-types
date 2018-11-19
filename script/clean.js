0e7582de720b6b1258b9b7a2f1427882c6d603c0
Brie <723547+brieb@users.noreply.github.com>
2018-11-18 20:03:49 -0800

    Clean emitted files for types.ts

--- script/clean.js
+++ script/clean.js
@@ -5,7 +5,7 @@ const ext = "{js,js.map,d.ts,d.ts.map}";
 
 glob.sync("{def,lib}/**/*." + ext)
   .concat(glob.sync("test{*,/!(data)/**}/*." + ext))
-  .concat(glob.sync("{fork,main}." + ext))
+  .concat(glob.sync("{fork,main,types}." + ext))
   .forEach(filename => {
     if (fs.existsSync(filename)) {
       fs.unlinkSync(filename);
