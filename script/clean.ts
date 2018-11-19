9f9558fb5e0f4a74b033fe9050d6c167a6fb07cd
Brie <723547+brieb@users.noreply.github.com>
2018-11-19 10:16:49 -0800

    Only emitted files in gen dir

--- script/clean.ts
+++ script/clean.ts
@@ -3,8 +3,7 @@ import glob from "glob";
 
 const ext = "{js,js.map,d.ts,d.ts.map}";
 
-glob.sync("{def,lib,script}/**/*." + ext)
-  .concat(glob.sync("gen/**/*"))
+glob.sync("{def,lib,script,gen}/**/*." + ext)
   .concat(glob.sync("test{*,/!(data)/**}/*." + ext))
   .concat(glob.sync("{fork,main,types}." + ext))
   .forEach(filename => {
