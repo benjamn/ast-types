78066588ac6f8a5038784d8f05d6a0bf09063f5b
Thomas Chetwin <tchetwin@bloomberg.net>
2021-02-15 12:04:19 +0000

    Test class features with babylon/babel/acorn
    Signed-off-by: Thomas Chetwin <tchetwin@bloomberg.net>

--- test/shared.ts
+++ test/shared.ts
@@ -2,45 +2,91 @@ import path from "path";
 import fs from "fs";
 import { parse as esprimaParse } from "esprima";
 import { parse as reifyBabylonParse } from "reify/lib/parsers/babylon";
+import { parse as rawBabelParse } from "@babel/parser";
+import { parse as acornParse } from "acorn";
 import { namedTypes as n } from "../main";
 
-export function validateECMAScript(file: any) {
-  var fullPath = path.join(__dirname, "..", file);
+export type Parser = "esprima" | "babylon" | "babel-parser" | "acorn";
 
-  it("should validate " + file + " with Esprima", function (done) {
-    fs.readFile(fullPath, "utf8", function(err, code) {
-      if (err) {
-        throw err;
-      }
+export function validateECMAScript(
+  file: string,
+  skipList?: Partial<Record<Parser, boolean>>
+) {
+  const fullPath = path.join(__dirname, "..", file);
+  const codeP = fs.promises.readFile(fullPath, "utf8");
 
-      n.Program.assert(esprimaParse(code), true);
-      n.Program.assert(esprimaParse(code, {
-        loc: true
-      }), true);
-
-      done();
-    });
+  it(`should validate ${file} with Esprima`, async function () {
+    if (skipList?.esprima) {
+      return;
+    }
+    const code = await codeP;
+    n.Program.assert(esprimaParse(code), true);
+    n.Program.assert(esprimaParse(code, {
+      loc: true
+    }), true);
   });
 
-  it("should validate " + file + " with Babylon", function (done) {
-    fs.readFile(fullPath, "utf8", function (err, code) {
-      if (err) {
-        throw err;
-      }
-      var ast = babylonParse(code);
-      n.Program.assert(ast, true);
-      done();
-    });
+  it(`should validate ${file} with Babylon`, async function () {
+    if (skipList?.babylon) {
+      return;
+    }
+    const code = await codeP;
+    var ast = babylonParse(code);
+    n.Program.assert(ast, true);
+  });
+
+  it(`should validate ${file} with babel`, async function () {
+    if (skipList?.["babel-parser"]) {
+      return;
+    }
+    const code = await codeP;
+    const ast = babelParse(code);
+    n.Program.assert(ast, true);
+  });
+
+  it(`should validate ${file} with acorn`, async function () {
+    if (skipList?.["acorn"]) {
+      return;
+    }
+    const code = await codeP;
+    const ast = acornStage3Parse(code, );
+    n.Program.assert(ast, true);
   });
 };
 
-export function babylonParse(source: any, options?: any) {
+export function babylonParse(source: string, options?: any) {
   var ast = reifyBabylonParse(source, options);
   if (ast.type === "File") ast = ast.program;
   return ast;
 }
 
 export { esprimaParse };
+
+function babelParse(source: string) {
+  const ast = rawBabelParse(source, {
+    sourceType: "module",
+    plugins: [
+      "typescript",
+      "objectRestSpread",
+      "classProperties",
+      "optionalCatchBinding",
+      "numericSeparator",
+      "optionalChaining",
+      "nullishCoalescingOperator",
+      "classPrivateProperties",
+      "classPrivateMethods",
+    ]
+  });
+  return ast.program;
+}
+
+function acornStage3Parse(source: string) {
+  const ast = acornParse(source, {
+    sourceType: "module",
+    ecmaVersion: "latest",
+  });
+  return ast;
+}
 
 // Helper for determining if we should care that a given type is not defined yet.
 // TODO Periodically revisit this as proposals advance.
