6ab0cca4b7a48bc5bee658dd4cae59273f8df198
Greg Price <greg@zulip.com>
2022-05-07 23:45:27 -0700

    flow: ObjectTypeProperty takes flags `static` and `method`
    These are already present in parser output.

--- def/flow.ts
+++ def/flow.ts
@@ -171,6 +171,8 @@ export default function (fork: Fork) {
     .field("key", or(def("Literal"), def("Identifier")))
     .field("value", def("FlowType"))
     .field("optional", Boolean)
+    .field("static", Boolean, defaults["false"])
+    .field("method", Boolean, defaults["false"])
     .field("variance", LegacyVariance, defaults["null"]);
 
   def("ObjectTypeIndexer")
